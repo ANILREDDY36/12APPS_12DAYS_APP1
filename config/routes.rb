84754c14346fd258923441edce2644c694c7ef68
ANILREDDY36 <anilroyal867@gmail.com>
2019-09-18 23:16:03 +0530

    add comments to links

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
       put "like",    to: "links#upvote"
       put "dislike", to: "links#downvote"
     end
+    resources :comments
   end
   root to: "links#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
